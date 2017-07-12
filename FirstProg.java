77fe3251188f502b88ab9a739d4147aceed2b6c1
Kittygomeowr <rkussery@gmail.com>
2017-07-12 13:14:24 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main(String [] args ) {
+		System.out.println( "Rosanne is determined to learn how to code.");
+		System.out.println( "Today's date is July 6th 2017");
+	}
+}
